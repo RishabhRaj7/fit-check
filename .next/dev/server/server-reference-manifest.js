95bfb4a5298f457a7c8a1a25b14f89b25c3fc03e
RishabhRaj7 <rishabhraj.txt@gmail.com>
2026-09-09 03:50:33 +0530

    stop tracking .next build output

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jOVcv6UGJrx9rQgldABzdq7dKUY3tIPiFC63JwPxjbg=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_0_12fqk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_hero_Scene_tsx_11mn_ez._.js","static/chunks/node_modules_0r6hged._.js","static/chunks/src_components_0zf5hyd._.js"],
-    source: "dynamic"
-});
